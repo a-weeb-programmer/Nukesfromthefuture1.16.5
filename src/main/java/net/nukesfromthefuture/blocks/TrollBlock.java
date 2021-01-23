01b1b5958599972ae4e479650bb96965ed103bc2
elite_mlgbro <you@example.com>
2021-01-23 14:38:42 -0700

    haha outdated memes go brrr

---
+++ src/main/java/net/nukesfromthefuture/blocks/TrollBlock.java
@@ -0,0 +1,12 @@
+package net.nukesfromthefuture.blocks;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class TrollBlock extends Block {
+    //haha outdated memes go brrrrrrrr
+    public TrollBlock(AbstractBlock.Properties prop){
+        super(prop);
+    }
+}
